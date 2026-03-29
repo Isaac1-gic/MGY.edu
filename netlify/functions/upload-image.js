72805a2382b120748c593249e285767d1c0b2a4e
Isaac Chitsakamire <isaacchitsakamire@gmail.com>
2026-03-29 13:35:49 +0300

    Enhance upload-image function with logging
    Added logging for debugging during image upload process.

--- netlify/functions/upload-image.js
+++ netlify/functions/upload-image.js
@@ -3,21 +3,25 @@ const cloudinary = require('cloudinary').v2;
 
 // Configure Cloudinary with Environment Variables 
 // (Set these in Netlify Console > Site Settings > Environment Variables)
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+
 
 export const handler = async (event) => {
+  console.log('Sarting')
   // Only allow POST requests
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
   try {
+    console.log('get img and id')
     const { image, userUid } = JSON.parse(event.body);
-
+    console.log('getting keys')
+    cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    console.log('uploading')
     // Upload to Cloudinary
     const result = await cloudinary.uploader.upload(image, {
       public_id: `profile_${userUid}`, // Using UID ensures the old one is overwritten
@@ -31,6 +35,7 @@ export const handler = async (event) => {
       body: JSON.stringify({ url: result.secure_url }),
     };
   } catch (error) {
+    console.log(error)
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
