992fce36d18c3addc6e0c1bee8cc3fc4e00cb73d
Isaac Chitsakamire <isaacchitsakamire@gmail.com>
2026-04-21 08:28:59 +0300

    Update app.py

--- render/app.py
+++ render/app.py
@@ -18,6 +18,8 @@ app = Flask(__name__)
 CORS(app, origins=[
     "https://mgy265.netlify.app/",
     "https://mgy265.netlify.app",
+    'https://isaac1-gic.github.io/MGY.edu/',
+    'https://isaac1-gic.github.io/MGY.edu',
     "http://localhost:5500",
     "http://127.0.0.1:5500"
 ])
