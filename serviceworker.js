1f285a5f982ad9465359dffe9336b7461dad548f
Isaac Chitsakamire <isaacchitsakamire@gmail.com>
2026-04-26 08:08:01 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'mgy-v4';
-const CACHE_EXTERNAL_NAME = 'external-assets-cache-v4';
+const CACHE_EXTERNAL_NAME = 'external-assets-cache-v5';
 const STUDY_TAG = 'Daily-edu-updates';
 
 const OFFLINE_URLS = [
