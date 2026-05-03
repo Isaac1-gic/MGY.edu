8e472ad2f74710256276ba6133e4fe6e0ea1a629
Isaac Chitsakamire <isaacchitsakamire@gmail.com>
2026-05-04 02:04:46 +0300

    Update app.py

--- render/app.py
+++ render/app.py
@@ -209,8 +209,8 @@ def ask_gemini():
                 obj = reply_text #parse_mgy_json(reply_text,chat)
                 if obj:
                     post = obj
-                    lustMsg = list(old_post.items())
-                    print(old_post)
+                    listMsg = list(old_post.items())
+                    print(listMsg)
                     lastMsg  = listMsg[-1]
                     print(listMsg)
                     mgyPostFormat = {
