d6d931a0d340a1788f4a22c21b9ceca271263833
Isaac Chitsakamire <isaacchitsakamire@gmail.com>
2026-05-19 07:11:34 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mgy-v1-05-2026-18-08:40';
+const CACHE_NAME = 'mgy-v1-05-2026-19';
 const CACHE_EXTERNAL_NAME = 'external-assets-cache-v5';
 const STUDY_TAG = 'Daily-edu-updates';
 
